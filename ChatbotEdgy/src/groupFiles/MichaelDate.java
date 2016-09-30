70cad7d09544c3b6a3e8fa0c041c1adf3d4dcd5a
Student 7 <Student 7@10.8.33.168>
2016-09-30 12:15:53 -0400

    New MichaelDate class.

---
+++ ChatbotEdgy/src/groupFiles/MichaelDate.java
@@ -0,0 +1,27 @@
+package groupFiles;
+
+public class MichaelDate implements Topic {
+	
+	private boolean inDateLoop;
+	private String dateResponse;
+	@Override
+	public void talk() {
+		// TODO Auto-generated method stub
+		inDateLoop = true;
+		
+	}
+
+	@Override
+	public boolean isTriggered(String userInput) {
+		String[] triggers ={"date","go out","chill","hang out"};
+		for (int i = 0; i<triggers.length-1; i++){
+			if (HansMain.findKeyword(userInput, triggers[i] , 0) >= 0){
+				return true;
+			}
+		}	
+		return false;
+	}
+	
+	
+}
+

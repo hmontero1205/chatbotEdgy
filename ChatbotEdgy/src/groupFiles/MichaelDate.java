14515249d9cb2262482050862b24f2a5a0f0e60c
mli8855 <mli8855@bths.edu>
2016-10-04 19:21:07 -0400

    Update MichaelDate.java
    continue from dateSimulate()

--- ChatbotEdgy/src/groupFiles/MichaelDate.java
+++ ChatbotEdgy/src/groupFiles/MichaelDate.java
@@ -4,16 +4,34 @@ public class MichaelDate implements Topic {
 	
 	private boolean inDateLoop;
 	private String dateResponse;
+	private String[] places = {"park","mall","beach","amusement park"};
+	private String[] events = {"a wild dog has appeared","it started raining","I lost my keys"};
 	
 	@Override
 	public void talk() {
 		// TODO Auto-generated method stub
 		inDateLoop = true;
 		while(inDateLoop){
-			HansMain.print("Testing. Let's on a date.");
+			HansMain.print("Well... I guess. Where would you like to go? I recommend the " + pickRandom(places) + ".");
 			dateResponse = HansMain.getInput();
+			if (HansMain.findKeyword(dateResponse, "yes" , 0) >= 0 || HansMain.findKeyword(dateResponse, "fine" , 0) >= 0 ){
+				HansMain.print("Sounds great! Let's go there now.");
+				dateSimulate();
+			}
+			else{
+				HansMain.print("You sure? I really want to go there...");
+				dateResponse = HansMain.getInput();
+				if (HansMain.findKeyword(dateResponse, "yes" , 0) >= 0 || HansMain.findKeyword(dateResponse, "fine" , 0) >= 0 ){
+					HansMain.print("Yay. I'm so excited.");
+					dateSimulate();
+				}
+				else{
+					HansMain.print("I guess we don't match after all... Sorry.");
+					inDateLoop = false;
+					HansMain.inLoop = true;			
+				}
+			}
 		}
-		
 	}
 
 	@Override
@@ -27,8 +45,15 @@ public class MichaelDate implements Topic {
 		return false;
 	}
 	
-	public void response(){
-		
+	public void dateSimulate(){
+		while (inDateLoop){
+			HansMain.print("Whew. That was a long walk.");
+			//Pull some of the events from the string array to make talk... continue off here...
+		}
+	}
+	
+	public String pickRandom(String[] arr){
+		return arr[(int) Math.random()*arr.length];
 	}
 }
 

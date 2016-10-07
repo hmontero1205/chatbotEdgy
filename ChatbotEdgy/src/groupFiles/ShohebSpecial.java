709cc0e321f47b31f53b9e4b85309899bba9e5f4
omesh <omesh@LAPTOP-RNDL7NT5>
2016-10-06 21:06:26 -0400

    some updates

--- ChatbotEdgy/src/groupFiles/ShohebSpecial.java
+++ ChatbotEdgy/src/groupFiles/ShohebSpecial.java
@@ -3,16 +3,15 @@ package groupFiles;
 //import chatBot.ShohebMain;
 
 public class ShohebSpecial implements Topic{
-	//private boolean inSpecialLoop;
+	private boolean inSpecialLoop;
 	static String currentInteraction;
-	static int possibleAt;
 	private String[] approvals = {"Sure!", "OK ^_^", "Gladly"};
 	private String[] politeRejections = {"Umm... I don't think I'm ready for that :/", "Sorry, I don't feel like it", "No thank you"};
 	private String[] angeryRejections = {"You're taking things too fast (>_<)", "You think I'm that easy huh?!", "No way jose"};
 	
 	public void talk() {
 		//inSpecialLoop = true;
-		if(HansMain.loveLevel >= possibleAt){
+		/*if(HansMain.loveLevel >= possibleAt){
 			HansMain.print(approvals[(int)(Math.random()*3)]);
 			HansMain.loveLevel++;
 			HansMain.frustrationLevel--;
@@ -25,6 +24,29 @@ public class ShohebSpecial implements Topic{
 			HansMain.print(angeryRejections[(int)(Math.random()*3)]);
 			HansMain.frustrationLevel++;
 			HansMain.loveLevel--;
+		}*/
+		inSpecialLoop = true;
+		while(inSpecialLoop){
+			switch (currentInteraction){
+				case "hold hands":
+					if(HansMain.loveLevel >= 3){
+						HansMain.print("Sure (^_^)");
+						HansMain.loveLevel++;
+					}
+					else{
+						HansMain.print("I don't feel like it");
+					}
+					break;
+				case "hug":
+					if(HansMain.loveLevel >= 4){
+						HansMain.print("Be gentle");
+					}
+					else{
+						HansMain.print("No thank you");
+					}
+					break;
+				
+			}
 		}
 	}
 
@@ -34,7 +56,6 @@ public class ShohebSpecial implements Topic{
 		for(int i = 0; i < triggers.length; i++){
 			if(HansMain.findKeyword(userInput, triggers[i], 0) >= 0){
 				currentInteraction = triggers[i];
-				possibleAt = i + 4;
 				return true;
 			}
 		}
